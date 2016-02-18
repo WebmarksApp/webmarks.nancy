da1fbf9bd39b8e8616345ae7cccee5e941662848
kingsor <avventureweb@gmail.com>
2016-02-18 16:27:51 +0100

    Initial commit

---
+++ webmarks.nancy/Modules/IndexModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace webmarks.nancy.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = _ => View["index"];
+        }
+    }
+}
